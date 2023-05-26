5e430eb2538b49a3a2d900a4a0b2152aa801a58b
DynamoUltima <dynamo.joey@gmail.com>
2023-05-26 11:11:17 +0000

    project modified

--- models/projects.ts
+++ models/projects.ts
@@ -1,66 +1,76 @@
-import { Schema, model, models ,Types} from "mongoose";
+import { Schema, model, models, Types } from "mongoose";
 
 
- const  mediaSchema =new Schema({
-   mediaId:{
-    type:Types.ObjectId
-   },
+const mediaSchema = new Schema({
+  mediaId: {
+    type: Types.ObjectId
+  },
 
-   dataType:{
+  dataType: {
+    type: String,
+    required: true
+  },
+  fileName: {
+    type: String
+  },
+  fileSize:{
     type:String,
-    required:true
-   },
 
-   link:{
-    type:String,
-    required:true,
-   },
-  
+  },
 
+  link: {
+    type: String,
+    required: true,
+  },
 
 
- } ,{ timestamps: { createdAt: 'created_on', updatedAt: 'updated_on' }})
+
+
+}, { timestamps: { createdAt: 'created_on', updatedAt: 'updated_on' } })
 
 
 const projectSchema = new Schema({
 
-    projectName: String,
-    projectImage:String,
-    description:String,
-    price:String,
-    expertise:String,
-    serviceType:{
-        type: [{
-            type: String,
-            enum: ['Thesis','Proposal','Assignment']
-        }],
-        default: ['Assignment']
-    },
-
-    duration:{
-      startDate:Date,
-      endDate:Date  
-    },
-    campus:String,
-    mediaData :{
-     type: [mediaSchema]
-    },
-    orderState: {
-      type: [{
-          type: String,
-          enum: ['pending','accepted','declined','completed']
-      }],
-      default: ['pending']
+  projectName: String,
+  projectImage: String,
+  description: String,
+  price: String,
+  expertise: String,
+  serviceType: {
+    type: [{
+      type: String,
+      enum: ['Thesis', 'Proposal', 'Assignment']
+    }],
+    default: ['Assignment']
   },
-  
-    createdBy:{
-        type:Types.ObjectId,
-        ref:'Client'
-    }
+  orderId:{
+    type:String,
+  },
 
-  
- 
-}, { timestamps: { createdAt: 'created_on', updatedAt: 'updated_on' }})
+  duration: {
+    startDate: Date,
+    endDate: Date
+  },
+  campus: String,
+  mediaData: {
+    type: [mediaSchema]
+  },
+  orderState: {
+    type: [{
+      type: String,
+      enum: ['pending', 'accepted', 'declined', 'completed']
+    }],
+    default: ['pending']
+  },
+
+  createdBy: {
+    type: Types.ObjectId,
+    ref: 'Client'
+  }
+
+
+
+}, { timestamps: { createdAt: 'created_on', updatedAt: 'updated_on' } })
 
 
 const Project = models.Project || model('Project', projectSchema);
---
+++ pages/api/projects/[getById].ts
@@ -0,0 +1,42 @@
+import cors from "cors";
+import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
+import Project from "../../../models/projects";
+import { runMiddleware } from "../../../utility/allowCors";
+import { connectMongo } from "../../../utils/connectMongo";
+
+
+const handler: NextApiHandler = async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<any>
+  ) {
+     
+
+    try {
+  
+      await connectMongo();
+      await runMiddleware(req, res, cors);
+      
+        //check if order Id exists
+        const {getById} = req.query
+        // const options = { "new": true };
+
+        
+       
+        const project =  await Project.findById(getById)
+        
+
+       return res.status(200).json({ message: 'Successful', project})
+
+   
+    //   res.status(200).json({ message: 'Unsucessful',  })
+ 
+    } catch (error) {
+      console.log(error);
+    return res.status(400).json({ error: error })
+    }
+   
+  }
+
+
+
+  export default handler;
